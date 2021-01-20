5e48eae56caf4535f9e498644f3baf1758008b6c
guanshuaijie <1845543324@qq.com>
2021-01-20 14:02:40 +0800

    feat:持续部署

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:12.20-alpine3.10
-COPY . /app
-WORKDIR /app
-RUN rm -rf node_modules
-RUN yarn --registry=https://registry.npm.taobao.org
-EXPOSE 3000
-CMD node ./app.js
--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-cur_sec=`date '+%s'`
-cur_ns=`date '+%N'`
-cur_timestamp=$((`date '+%s'`*1000+`date '+%N'`/1000000))
+set -e
 
-docker image build -t luyuan221/static-server:$cur_timestamp .
-docker login -u luyuan221 --password shuai2121
-docker image push luyuan221/static-server:$cur_timestamp
-echo 'build success'
+REMOTE_USERNAME="root"
+REMOTE_HOST="49.235.241.244"
+
+echo "Signing in to server..."
+
+ssh -tt -i ~/.ssh/id_rsa_travis $REMOTE_USERNAME@$REMOTE_HOST << EOF
+cd /usr/share/nginx/html/web/node-static-server && git pull && rm -rf node_modules && yarn --registry=https://registry.npm.taobao.org
+pm2 delete node-static-server
+pm2 start app.js --name node-static-server
+
+exit
+EOF
